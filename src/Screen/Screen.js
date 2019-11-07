460d2cee0aa4184cc0e66ac3b82c1d1c5184fc80
Justin Pippin <justinpippin15@gmail.com>
2019-11-07 12:35:43 -0600

    Added in the ComputationScreen component, styled it, and rendered it in the Screen component

--- src/Screen/Screen.js
+++ src/Screen/Screen.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './Screen.css';
 import ResultsScreen from '../Components/ResultsScreen/ResultsScreen';
+import ComputationScreen from '../Components/ComputationScreen/ComputationScreen';
 
 /* Component that holds the results screen and calculation screen */
 class Screen extends Component {
     render() {
         return (
             <div className="Screen">
-            <ResultsScreen>
-                0
-            </ResultsScreen>
+            <ResultsScreen>0</ResultsScreen>
+            <ComputationScreen>2+3</ComputationScreen>
             </div>
         );
     }
